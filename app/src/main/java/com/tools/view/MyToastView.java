d0fb2d8427d811ffc649524ddb886ab52162fdea
Milo <hzmeibi@gmail.com>
2017-12-13 16:28:40 +0800

    版本编号：0.1.2 更新时间：2017-12-13 更新内容：         1.MyToastView 支持中文         2.MyProgressDialog 支持中文 新增内容：

--- app/src/main/java/com/tools/view/MyToastView.java
+++ app/src/main/java/com/tools/view/MyToastView.java
@@ -1,6 +1,7 @@
 package com.tools.view;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tools.R;
+
+import static android.R.attr.id;
 
 
 /**
@@ -62,13 +65,55 @@ public class MyToastView extends Toast {
     }
 
     /**
+     * 自定义Toast样式
+     *
+     * @param resId
+     * @param text
+     * @param duration
+     * @description
+     */
+    private Toast makeText(int resId, String text, int duration) {
+        Toast result = new Toast(mContext);
+        // 获取LayoutInflater对象
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // 由layout文件创建一个View对象
+        View layout = inflater.inflate(R.layout.view_my_toast, null);
+
+        // 实例化ImageView和TextView对象
+        ImageView imageView = (ImageView) layout.findViewById(R.id.ImageView);
+        TextView textView = (TextView) layout.findViewById(R.id.message);
+
+        //这里我为了给大家展示就使用这个方面既能显示无图也能显示带图的toast
+        if (resId == 0) {
+            imageView.setVisibility(View.GONE);
+        } else {
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageResource(resId);
+        }
+        textView.setText(text);
+        result.setView(layout);
+        result.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        result.setDuration(duration);
+
+        return result;
+    }
+
+    /**
      * show toast
-     * 强制使用ID 方便维护
      *
      * @paramid
      */
     public void show(int id) {
         show(0, id, 100);
+    }
+
+    /**
+     * show toast
+     *
+     * @paramid
+     */
+    public void show(String str) {
+        show(0, str, 100);
     }
 
     /**
@@ -88,6 +133,19 @@ public class MyToastView extends Toast {
      * show toast
      *
      * @param drawableId
+     * @param str
+     */
+    public void show(int drawableId, String str) {
+        mToast = mMyToastView.makeText(drawableId, str, 100);
+        mToast.show();
+        mId = id;
+        show(drawableId, id, 100);
+    }
+
+    /**
+     * show toast
+     *
+     * @param drawableId
      * @param id
      * @param duration
      */
@@ -96,6 +154,23 @@ public class MyToastView extends Toast {
             mToast.cancel();
         }
         mToast = mMyToastView.makeText(drawableId, id, duration);
+        mToast.show();
+        mId = id;
+    }
+
+
+    /**
+     * show toast
+     *
+     * @param drawableId
+     * @param str
+     * @param duration
+     */
+    public void show(int drawableId, String str, int duration) {
+        if (mToast != null && !TextUtils.isEmpty(str)) {
+            mToast.cancel();
+        }
+        mToast = mMyToastView.makeText(drawableId, str, duration);
         mToast.show();
         mId = id;
     }
